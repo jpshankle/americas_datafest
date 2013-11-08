de5ab13b3c8751f3c2a93b8e8756c57b5a69ac9e
Jeremy Shankle <jshankle@skylinenet.net>
2013-11-08 08:30:47 -0500

    add bootstrap glyphicons

--- client/src/dashboard/controller.js
+++ client/src/dashboard/controller.js
@@ -1,5 +1,9 @@
 app.controller('DashboardCtrl', ['$scope', '$rootScope', function ($scope, $rootScope) {
+  
     $scope.rice = 12;
+    $scope.numberOfItems = function (items) {
+        return new Array(items);
+    };
     $rootScope.$watch('countries.selectedCountry', function() {
        $scope.lineData = $scope.countries.countryData[$rootScope.countries.selectedCountry.id];
     }, true);
