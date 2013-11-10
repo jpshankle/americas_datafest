bab5c791de7bb37d85c292fe82f62f7bbcadb198
Michael Branan <mbranan@skylinenet.net>
2013-11-10 01:46:55 -0500

    styling

--- server/Gruntfile.js
+++ server/Gruntfile.js
@@ -58,6 +58,7 @@ module.exports = function (grunt) {
                 src: [
                     '**/*.html',
                     '**/*.png',
+                    '**/*.gif',
                     '**/*.json',
                     '*.tsv',
                     '*.csv',
