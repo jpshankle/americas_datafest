b0918f66388def3264a5658a89c5fcd5d80a1463
Jeremy Shankle <jshankle@skylinenet.net>
2013-11-09 23:35:15 -0500

    more things

--- client/src/stories/controller.js
+++ client/src/stories/controller.js
@@ -1,6 +1,7 @@
 app.controller('StoriesCtrl', ['$scope', '$rootScope', '$http', function ($scope, $rootScope, $http) {
     $rootScope.immivizContentAnimateClass = 'back-button';
     d3.selectAll('.globeElement').selectAll('.active').classed('active', false);
+    $rootScope.countries.selectedCountry = null;
     $http({
     	method: 'GET',
     	url: '/stories/stories.json'
